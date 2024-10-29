3b1b9b6dadeeb3b381c543dfad3f7e32d26dee16
Sunil <sunil.nunubilli@gmail.com>
2024-10-29 20:09:59 +0530

    shell parctice

--- 09-installations.sh
+++ 09-installations.sh
@@ -6,13 +6,14 @@ if [ $USERID -gt 0 ]
 THEN 
     echo "User didnt have req priviliges to run this script"
     exit 1
-fi 
+fi
 
 dnf installed mysql -y
 
 if [ $? -ne 0 ]
 THEN
     echo "Mysql is not installed yet, installing it now"
+    dnf install mysql
     exit 1
 
 ELSE
