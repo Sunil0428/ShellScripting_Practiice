077defc91b8fa87d8bcce48d16504655f1b79a5d
Sunil <sunil.nunubilli@gmail.com>
2024-10-29 16:54:00 +0530

    shell parctice

--- 04-read.sh
+++ 04-read.sh
@@ -6,7 +6,7 @@ read PERSON1
 echo "Enter PERSON2"
 read PERSON2
 
-echo "${person1} : Hi Helloo "
-echo "${person2} :Helloo"
-echo "${person1} :How are you?"
-echo "${person2} :I m good , you?"
+echo "${PERSON1} : Hi Helloo "
+echo "${PERSON2} :Helloo"
+echo "${PERSON1} :How are you?"
+echo "${PERSON2} :I m good , you?"
