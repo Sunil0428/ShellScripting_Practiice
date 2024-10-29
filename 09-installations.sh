e9add0dc092249fcd42d48259673de7f6c2e3161
Sunil <sunil.nunubilli@gmail.com>
2024-10-29 20:55:20 +0530

    shell parctice

--- 09-installations.sh
+++ 09-installations.sh
@@ -1,26 +1,39 @@
-!#/bin/bash
+#!/bin/bash
 USERID=$(id -u)
 
-if [ $USERID -gt 0 ]
-then 
-    echo "User didnt have req priviliges to run this script"
-    exit 1
-fi
+R="\e[31m"
+G="\e[32m"
+N="\e[33"
+
+CHECKUSER()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$R please use the root access for running the script $N"
+    fi
+}
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "$R there is an issue with $2 intallation $N "
+        exit 1
+    else
+        echo "$G $2 installation is success $N"
+    fi
+}
+
+CHECKUSER $USERID
 
 dnf install mysql -y
 
 if [ $? -ne 0 ];
     then
-        echo "Mysql is not installed yet, installing it now"
+        echo "$R Mysql is not installed yet, installing it now $N"
         dnf install mysql
-            if [ $? -ne 0 ]
-            then
-                echo "there is an issue with mysql intallation"
-                exit 1
-            else
-                 echo "mysql installation is success"
-            fi
+        VALIDATE $? "mysql"
          exit 1
     else
-        echo "Mysql is already installed , nothing to do"
+        echo "$G Mysql is already installed , nothing to do $N "
 fi 
