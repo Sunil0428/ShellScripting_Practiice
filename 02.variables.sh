3b92c4daaf2842929cd129ef976a3d3416023252
Sunil <sunil.nunubilli@gmail.com>
2024-10-29 16:18:01 +0530

    shell parctice

--- 02.variables.sh
+++ 02.variables.sh
@@ -2,7 +2,7 @@
 person1="Sunil"
 person2="Bhuvana"
 
-echo "$person1 : Hi Helloo "
-echo "$person2 :Helloo"
-echo "$person1 :How are you ?"
-echo "$person2 :I m good , you ?"
+echo "${person1} : Hi Helloo "
+echo "${person2} :Helloo"
+echo "${person1} :How are you ?"
+echo "${person2} :I m good , you ?"
