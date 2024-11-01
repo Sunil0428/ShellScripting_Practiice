17cfa31ae59090250ef031cbb2c8fd9622208925
Sunil <sunil.nunubilli@gmail.com>
2024-11-02 01:44:45 +0530

    shell parctice

--- 11.Logfile.sh
+++ 11.Logfile.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 LOGPATH="/var/log/Shell-scripting"
 LOGFILENAME= $($0 | cut -d "." -f1)
-TIMESTAMP= $date 
-LOGFILE="$LOGPATH/$LOGFILENAME-$TIMESTAMP"
-echo "$LOGFILE"
+# TIMESTAMP= $date 
+# LOGFILE="$LOGPATH/$LOGFILENAME-$TIMESTAMP"
+echo "$LOGFILENAME"
