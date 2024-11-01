2cf1c5358f741b15524a957e4b1bf587a7e0f4ec
Sunil <sunil.nunubilli@gmail.com>
2024-11-02 00:52:52 +0530

    shell parctice

--- 10-loops.sh
+++ 10-loops.sh
@@ -1,4 +1,39 @@
-for i in {1..10}
-do
-  echo $i
-done
+
+USERID=$(id -u)
+R="\e[31m"
+B="\e[32m"
+N="\e[0m"
+
+CHECKUSER()
+{
+    if [ $1 -ne 0 ]
+    then    
+        echo -e "$R Please use the rrot access to run this script $N"
+        exit 1
+}
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then    
+        echo -e "$R there is a problem in installing $2 $N"
+    else
+        echo -e "$B $2 installation is complete $N"
+    fi
+}
+
+CHECKUSER $USERID
+
+for package in $@
+{
+    dnf installed $package
+    if [ $? -ne 0 ]
+    then  
+        echo -e "$R $package is not there in the system, so installing it $N"
+        dnf install $package
+        VALIDATE $? $package
+    else
+        echo -e "$B $package is already there in the system, nothing to do $N"
+    fi
+}
+
